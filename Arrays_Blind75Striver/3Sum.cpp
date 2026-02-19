16020f350915949eaea9fa84ff195929b392a646
Vivek Sharma <vivek.codespace@gmail.com>
2026-02-19 20:33:56 +0530

    change the folder name

--- Arrays_Blind75Striver/3Sum.cpp
+++ Arrays_Blind75Striver/3Sum.cpp
@@ -51,3 +51,38 @@ public:
     }
 };
 
+//More better solution extra time complecity was introduced due to .find method 
+//Works in  O(n^2) time complexity
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> st;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-2;i++)
+        {
+            int low=i+1,high=n-1;
+            while(low<high)
+            {
+                int temp=nums[i]+nums[low]+nums[high];
+                if(temp<0)
+                {
+                    low++;
+                }
+                else if(temp>0)
+                {
+                    high--;
+                }
+                else {
+                    vector<int> v{nums[i],nums[low],nums[high]};
+                    st.insert(v);
+                    low++;
+                    high--;
+                }
+                
+            }
+        }
+        vector<vector<int>> vec(st.begin(),st.end());
+        return vec;
+    }
+};
