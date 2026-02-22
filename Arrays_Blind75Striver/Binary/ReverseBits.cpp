b7c27e5276abd8c83d139fcd82fb4916104d9f1f
Vivek Sharma <vivek.codespace@gmail.com>
2026-02-22 15:44:39 +0530

    Reverse Bits question

---
+++ Arrays_Blind75Striver/Binary/ReverseBits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i=0;i<32;i++){
+            res = res<<1;
+            res = (n&1) | res;
+            n = n>>1;  
+        }
+        return res;
+    }
+};
