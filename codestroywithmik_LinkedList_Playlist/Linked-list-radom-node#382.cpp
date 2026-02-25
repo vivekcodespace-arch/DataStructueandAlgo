da243a9857fc23c58a7b718df9fefd3309f250f0
Vivek Sharma <vivek.codespace@gmail.com>
2026-02-25 10:53:17 +0530

    made Linked list ranodm no. prob using brute force approach

---
+++ codestroywithmik_LinkedList_Playlist/Linked-list-radom-node#382.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    //Brute force approach 
+    // Store the linked list in an array and return the random no. from the array
+    vector<int> arr;
+    mt19937 gen;
+    Solution(ListNode* head) {
+        ListNode* temp = head;
+        while (temp) {
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+    }
+
+    int getRandom() {
+        uniform_int_distribution<int> dist(0, arr.size() - 1);
+        return arr[dist(gen)];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
