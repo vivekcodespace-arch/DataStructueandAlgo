17380ba25e4f3bc51b876f9c8d7ca4865bb192ca
Vivek Sharma <vivek.codespace@gmail.com>
2026-02-26 10:21:56 +0530

    done reservoir sampling

--- codestroywithmik_LinkedList_Playlist/Linked-list-radom-node#382_REV.cpp
+++ codestroywithmik_LinkedList_Playlist/Linked-list-radom-node#382_REV.cpp
@@ -33,4 +33,47 @@ public:
  * Solution* obj = new Solution(head);
  * int param_1 = obj->getRandom();
  */
+
+
+//2nd approach ..need to revise it
 //Reservoir sampling method the better approach to this prob
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* Head;
+    Solution(ListNode* head) {
+        Head = head;
+    }
+    
+    int getRandom() {
+        int count = 1 ;
+        int result = 0;
+
+        ListNode* temp = Head;
+        while(temp!=NULL){
+            int x = rand();
+            if(x % count < 1.0/count){
+                result = temp->val;
+            }
+            cout<<x<<" ";
+            count++;
+            temp = temp->next;
+        }
+        return result;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
