e329467192fa22ac8ec6593e1a557d9e8be83265
Vivek Sharma <vivek.codespace@gmail.com>
2026-03-27 18:09:48 +0530

    made BFS traversal of the bipartite graph

--- Graph_codestorywithmik/BipartitieGraph.cpp
+++ Graph_codestorywithmik/BipartitieGraph.cpp
@@ -1,3 +1,5 @@
+
+//DFS solution
 class Solution {
   public:
   
@@ -32,6 +34,55 @@ class Solution {
         }
         for(int i=0;i<V;i++){
             if(colors[i]==-1 && solve(i,1,colors,mp)==false){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
+//BFS solution
+class Solution {
+  public:
+  
+  
+    bool BFS(int current_node, int current_color, vector<int>& colors, unordered_map<int,vector<int>> &mp){
+        queue<int> q;
+        q.push(current_node);
+        colors[current_node] = current_color;
+        
+        
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(auto& v:mp[u]){
+                if(colors[v] == colors[u]){
+                    return false;
+                }
+                else if(colors[v] == -1){
+                    
+                    colors[v] = 1 - colors[u];
+                    q.push(v);
+                }
+            }
+        }
+        
+        return true;
+    }
+    bool isBipartite(int V, vector<vector<int>> &edges) {
+        // Code here
+        
+        unordered_map<int,vector<int>> mp;
+        vector<int> colors(V,-1);
+        for(auto& x: edges){
+            int u =x[0];
+            int v = x[1];
+            
+            mp[u].push_back(v);
+            mp[v].push_back(u);
+        }
+        for(int i=0;i<V;i++){
+            if(colors[i]==-1 && BFS(i,1,colors,mp)==false){
                 return false;
             }
         }
