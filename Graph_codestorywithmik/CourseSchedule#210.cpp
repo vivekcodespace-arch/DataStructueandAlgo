f6037e95fae826506c9ba8d450efdbde141bf387
Vivek Sharma <vivek.codespace@gmail.com>
2026-03-27 11:08:21 +0530

    made course schedule 210 using DFS traversal

--- Graph_codestorywithmik/CourseSchedule#210.cpp
+++ Graph_codestorywithmik/CourseSchedule#210.cpp
@@ -40,3 +40,53 @@ public:
     }
 };
 
+//Using DFS traversal and topological sorting via DFS traversal
+class Solution {
+public:
+    stack<int> st; // used to store the answer
+    bool isCycleDFS(int u, unordered_map<int,vector<int>>& mp, vector<bool>& visited, vector<bool>& inRecursion){
+        
+        visited[u]  = true;
+        inRecursion[u] = true;
+        for(auto& v: mp[u]){
+            if(!visited[v]){
+                if(isCycleDFS(v,mp,visited,inRecursion)){
+                    return true;
+                }
+            }
+            if(inRecursion[v] == true){
+                return true;
+            }
+        }
+        st.push(u);
+        inRecursion[u]  = false;
+        return false;
+        
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> mp;
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
+
+        for (auto& x : prerequisites) {
+            int u = x[1];
+            int v = x[0];
+            mp[u].push_back(v);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i] && isCycleDFS(i,mp,visited,inRecursion)){
+                return {};
+            }
+        }
+        vector<int> ans;
+        
+        while(!st.empty()){
+            int top = st.top();
+            st.pop();
+            ans.push_back(top);
+        }
+
+        return ans;
+    }
+};
+
