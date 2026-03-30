bb158b95904c8336c2f4ec9a0f121a72d3082bf9
Vivek Sharma <vivek.codespace@gmail.com>
2026-03-30 23:01:29 +0530

    made the dsu solution

--- Graph_codestorywithmik/NumbreOfOperatoinToMakeNetworkConnected#1319.cpp
+++ Graph_codestorywithmik/NumbreOfOperatoinToMakeNetworkConnected#1319.cpp
@@ -42,3 +42,56 @@ public:
         return group-1;
     }
 };
+//DSU solution
+
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+    int find(int i, vector<int>& parent){
+        if(parent[i] == i) return i;
+
+        return parent[i] = find(parent[i], parent);
+    }
+    bool Union(int x, int y, vector<int>& parent, vector<int>& rank){
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+
+        if(x_parent != y_parent){
+
+            if(rank[x_parent] > rank[y_parent]){
+                parent[y_parent] = x_parent;
+            }
+            else if(rank[x_parent] < rank[y_parent]){
+                parent[x_parent] = y_parent;
+            }
+            else{
+                parent[x_parent] = y_parent;
+                rank[y_parent]+=1;
+            }
+            return true;
+        }
+
+        return false;
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& connections) {
+
+        if(connections.size() < n-1) return -1;
+
+        rank.resize(n,0);
+        for(int i=0;i<n;i++){
+            parent.push_back(i);
+        }
+        int x = n-1;
+        for(auto& connection: connections){
+            int u = connection[0];
+            int v = connection[1];
+            if(Union(u, v, parent, rank)){
+                x--;
+            }
+        }
+        return x;
+
+    }
+};
