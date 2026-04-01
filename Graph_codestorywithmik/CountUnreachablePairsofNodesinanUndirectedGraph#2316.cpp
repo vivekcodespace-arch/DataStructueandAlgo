741b5f96da92d4b1be33945d84d955a48a0a7927
Vivek Sharma <vivek.codespace@gmail.com>
2026-04-01 19:41:21 +0530

    made the solution usig DSU

--- Graph_codestorywithmik/CountUnreachablePairsofNodesinanUndirectedGraph#2316.cpp
+++ Graph_codestorywithmik/CountUnreachablePairsofNodesinanUndirectedGraph#2316.cpp
@@ -156,3 +156,66 @@ public:
         return result;
     }
 };
+
+
+//Solution using disjoint set union
+
+class Solution {
+public:
+    vector<int> parent;
+    vector<int> rank;
+    int find(int i, vector<int>& parent){
+        if(parent[i] == i) return i;
+
+        return parent[i] = find(parent[i], parent);
+    }
+    void Union(int x, int y, vector<int>& parent, vector<int>& rank){
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else{
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        //now i have to implement it via DSU
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+
+        //firstly iterate in the edges and then union them
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            Union(u,v,parent,rank);
+        }
+
+        //then after apply the find operation and make a map
+        unordered_map<int, int> mp;
+        for(int i=0;i<n;i++){
+            int temp = find(i,parent);
+            mp[temp]+=1;
+        }
+        int total_nodes = n;
+        long long result = 0;
+        for(auto& x:mp)
+        {
+            int temp = x.second;
+            result += (long long)temp * (total_nodes-temp);
+            total_nodes = total_nodes - temp;
+        }
+
+        return result;
+
+    }
+};
