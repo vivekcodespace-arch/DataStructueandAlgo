a05784266c56fb91866c192ef0d02af827cd02a7
Vivek Sharma <vivek.codespace@gmail.com>
2026-04-02 20:33:21 +0530

    made the solution using set

--- Graph_codestorywithmik/DijkstraAlgo.cpp
+++ Graph_codestorywithmik/DijkstraAlgo.cpp
@@ -42,3 +42,51 @@ class Solution {
         
     }
 };
+//More optimized solution using set
+
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<int> result(V, INT_MAX);
+        unordered_map<int, vector<pair<int,int> >> mp;
+        set<pair<int,int>> st;
+        
+        for(auto& edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            
+            mp[u].push_back({v,wt}); //node , weight
+            mp[v].push_back({u,wt}); // node, weight
+        }
+        
+        result[src] = 0;
+        st.insert({0,src}); // distance, node
+        
+        while(!st.empty()){
+            auto& it = *st.begin();
+            int dist = it.first;
+            int nd = it.second;
+            st.erase(it);
+            
+            for(auto& x: mp[nd]){
+                int node = x.first;
+                int weight = x.second;
+                if(weight + dist < result[node]){
+                    
+                    if(result[node]!=INT_MAX){
+                        st.erase({result[node], node});
+                    }
+
+                    result[node] = weight+dist;
+                    st.insert({weight+dist, node});
+                }
+                
+            }
+        }
+        
+        return result;
+        
+    }
+};
