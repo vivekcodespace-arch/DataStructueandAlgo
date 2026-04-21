d0e2a20f56a3d391850cf06c4b05a036226654af
Vivek Sharma <vivek.codespace@gmail.com>
2026-04-21 12:59:28 +0530

    made DFS sol

---
+++ GraphPopularINterviewQuestoins_codestorywithmik/PossibleBipartition#886.cpp
@@ -0,0 +1,44 @@
+//This is was the DFS soliution time complexity O(V+E) 
+//Problem link: https://leetcode.com/problems/possible-bipartition/
+
+class Solution {
+public:
+    bool bipartite(int u, int i, vector<int>& colors, unordered_map<int, vector<int>>& mp) {
+        colors[u] = i;
+
+        for(auto& v: mp[u]){
+            if(colors[v] == colors[u]){
+                return false;
+            }
+            if(colors[v] == -1){
+                if(!bipartite(v,1-i,colors,mp)) return false;
+            }
+        }
+
+        return true;
+    }
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        //simple implementation of bipartite graph
+        //make the adjacency list 
+        unordered_map<int, vector<int>> mp;
+        for(auto& edge: dislikes){
+            int u = edge[0];
+            int v = edge[1];
+
+            mp[u].push_back(v);
+            mp[v].push_back(u);
+        }
+
+        vector<int> colors(n+1,-1);
+
+        for(int i = 1;i<=n;i++){
+            if(colors[i] == -1){
+                if(!bipartite(i,1,colors,mp))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
